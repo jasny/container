ff79772875d1e15e165c3e927eaa3dcaece1512b
Arnold Daniels <arnold@jasny.net>
2018-11-23 02:41:26 +0100

    Explain autowiring in interface

--- src/AutowireContainerInterface.php
+++ src/AutowireContainerInterface.php
@@ -6,14 +6,18 @@ use Psr\Container\ContainerInterface;
 
 /**
  * Container that support autowiring.
+ *
+ * Autowiring resolved dependencies automatically by looking at the type and/or other
+ * meta like phpdoc parameter info. These dependencies are loaded from the container
+ * and passed to the constructor.
  */
 interface AutowireContainerInterface extends ContainerInterface
 {
     /**
      * Instantiate a new object, autowire dependencies.
      *
-     * @param string $class
-     * @param mixed  ...$args
+     * @param string $class    Class name
+     * @param mixed  ...$args  Non-autowired arguments
      * @return object
      */
     public function autowire(string $class, ...$args);
