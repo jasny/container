db185f1cbb93e8d4dd17cf424b629a712b5d26ee
Arnold Daniels <arnold@jasny.net>
2020-04-19 07:41:09 +0200

    Replace dynamicReturnTypeMeta.json with .phpstorm.meta.php [skip ci]

---
+++ .phpstorm.meta.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * PhpStorm code completion
+ *
+ * Add code completion for PSR-11 Container Interface
+ */
+
+namespace PHPSTORM_META {
+    // PSR-11 Container Interface
+    override(\Psr\Container\ContainerInterface::get(0),
+        map([
+            '' => '@',
+        ])
+    );
+}
+
